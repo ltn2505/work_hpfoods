d5c119d6f6b5435a9de465389af5caab94d01b87
Kiet-LPA <kietlpa.work@gmail.com>
2025-08-15 18:06:29 +0700

    login/register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,14 +34,26 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
+            <!-- Register Button -->
+            <a href="{{ route('register') }}"
+            class="ms-3 inline-flex items-center px-4 py-2 bg-black-900 border border-transparent rounded-md font-semibold text-xs underline text-black uppercase tracking-widest hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition ease-in-out duration-150">
+                {{ __('Register') }}
+            </a>
+
+            <!-- Login Button -->
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+
+
+
     </form>
 </x-guest-layout>
